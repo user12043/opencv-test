46b794c42b892126b4cf1e8fbdfebfb9829b0cb5
user12043 <muaz.karatas@ogr.sakarya.edu.tr>
2018-09-06 14:12:22 +0300

    added a way to set -Djava.library.path relatively

--- src/ogr/user12043/opencv/test/Main.java
+++ src/ogr/user12043/opencv/test/Main.java
@@ -13,6 +13,21 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         try {
+            // Set library path relatively
+            /*java.io.File libPathLinux = new java.io.File("lib" + java.io.File.separator + "opencv-linux");
+            java.io.File libPathWindows = new java.io.File("lib" + java.io.File.separator + "opencv-windows" + java.io.File.separator + "x64");
+            String libraryPath = System.getProperty("java.library.path") + java.io.File.pathSeparator + libPathLinux.getAbsolutePath() + java.io.File.pathSeparator + libPathWindows.getAbsolutePath();
+            System.setProperty("java.library.path", libraryPath);
+            java.lang.reflect.Field fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
+            fieldSysPath.setAccessible(true);
+            fieldSysPath.set(null, null);*/
+
+            // Instead of this, run the program with jvm argument (requires absolute path in this case):
+            // -Djava.library.path=/path/to/opencv/library/file
+            // e.g:
+            // -Djava.library.path=C:\Users\ME99735\Desktop\Folder\projects\java\NonToyota\opencv-test\lib\opencv-windows\x64
+
+
             System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
             SwingUtilities.invokeLater(() -> new CameraDisplay().setVisible(true));
         } catch (Exception e) {
