148544a6aa7365333284aa44a293b02c0b71d239
user12043 <muaz.karatas@ogr.sakarya.edu.tr>
2018-09-10 17:28:56 +0300

    doc2: opencv basics completed

--- src/main/java/ogr/user12043/opencv/test/basics/BasicsController.java
+++ src/main/java/ogr/user12043/opencv/test/basics/BasicsController.java
@@ -5,17 +5,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import ogr.user12043.opencv.test.Utils;
 import ogr.user12043.opencv.test.Constants;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.Rect;
+import ogr.user12043.opencv.test.Utils;
+import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -36,6 +37,9 @@ public class BasicsController {
 
     @FXML
     private ImageView imageView_display;
+
+    @FXML
+    private ImageView imageView_histogram;
 
     @FXML
     private CheckBox checkBox_grayScale;
@@ -102,7 +106,51 @@ public class BasicsController {
         }
 
         imageView_display.setImage(SwingFXUtils.toFXImage(Utils.matToBufferedImage(frame), null));
-//        imageView_display.setImage(SwingFXUtils.toFXImage(cameraTest.getImage(), null));
+        updateHistogram(frame);
+    }
+
+    private void updateHistogram(Mat frame) {
+        List<Mat> rgbImages = new ArrayList<>();
+        Core.split(frame, rgbImages);
+
+        MatOfInt histSize = new MatOfInt(256);
+        MatOfInt channels = new MatOfInt(0);
+        MatOfFloat histRange = new MatOfFloat(0, 256);
+
+        Mat histR = new Mat();
+        Mat histG = new Mat();
+        Mat histB = new Mat();
+
+        Imgproc.calcHist(rgbImages.subList(0, 1), channels, new Mat(), histR, histSize, histRange, false);
+
+        if (!checkBox_grayScale.isSelected()) {
+            Imgproc.calcHist(rgbImages.subList(1, 2), channels, new Mat(), histG, histSize, histRange, false);
+            Imgproc.calcHist(rgbImages.subList(2, 3), channels, new Mat(), histB, histSize, histRange, false);
+        }
+
+        int histWidth = 150;
+        int histHeight = 150;
+        int binWidth = (int) Math.round(histWidth / histSize.get(0, 0)[0]);
+
+        Mat histImage = new Mat(histHeight, histWidth, CvType.CV_8UC3, new Scalar(0, 0, 0));
+        Core.normalize(histR, histR, 0, histImage.rows(), Core.NORM_MINMAX, -1, new Mat());
+
+        if (!checkBox_grayScale.isSelected()) {
+            Core.normalize(histG, histG, 0, histImage.rows(), Core.NORM_MINMAX, -1, new Mat());
+            Core.normalize(histB, histB, 0, histImage.rows(), Core.NORM_MINMAX, -1, new Mat());
+        }
+
+        for (int i = 1; i < histSize.get(0, 0)[0]; i++) {
+            Imgproc.line(histImage, new Point(binWidth * (i - 1), histHeight - Math.round(histR.get(i - 1, 0)[0])), new Point(binWidth * i, histHeight - Math.round(histR.get(i, 0)[0])), new Scalar(0, 0, 255), 2, 8, 0);
+
+            if (!checkBox_grayScale.isSelected()) {
+                Imgproc.line(histImage, new Point(binWidth * (i - 1), histHeight - Math.round(histG.get(i - 1, 0)[0])), new Point(binWidth * i, histHeight - Math.round(histG.get(i, 0)[0])), new Scalar(0, 255, 0), 2, 8, 0);
+                Imgproc.line(histImage, new Point(binWidth * (i - 1), histHeight - Math.round(histB.get(i - 1, 0)[0])), new Point(binWidth * i, histHeight - Math.round(histB.get(i, 0)[0])), new Scalar(255, 0, 0), 2, 8, 0);
+            }
+        }
+
+        Image histImg = SwingFXUtils.toFXImage(Utils.matToBufferedImage(histImage), null);
+        imageView_histogram.setImage(histImg);
     }
 
     private void initService() {
